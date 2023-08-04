068db46b8960a0104d18e28fde4fdbabdf9c237a
Anmisrom <rc7581206@gmail.com>
2023-08-04 08:28:56 +0700

    new push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<HomeLogin />} />
         <Route path="/All" element={<Allkategori />} />
+        <Route path="/allkategl" element={<Allkategori />} />
         <Route path="/login" />
         //Route Components
         <Route path="/profilmenu" element={<ProfileMenu />} />
