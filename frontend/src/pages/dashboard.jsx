a25797128898bdf95cbd9eb60cc685c53bb03b36
AAlby99 <albyrahmadani4@gmail.com>
2023-08-13 23:18:48 +0700

    membuat halaman dashboard (kurang grafik)

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import SidebarProfile from "../components/layout/sidebar/sidebar";
 import NavigationBar from "../components/layout/navigationBar/navigationBar";
+import Dpi from "../components/content/dashboard";
+import MyFooter from "../components/layout/footer/footer";
 
 function DashboardProfile() {
   const [activeMenu, setActiveMenu] = useState("Dashboard");
@@ -12,9 +14,11 @@ function DashboardProfile() {
       <div className="flex-row flex">
         <SidebarProfile activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
         <main className="flex-1 p-8">
-          <h1>Hai Gaes, ini halaman Dashboard</h1>
+          <Dpi/>
         </main>
+        
       </div>
+      <MyFooter/>
     </>
   );
 }
