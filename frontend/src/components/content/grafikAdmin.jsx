c00f29582635c94042fdd64e164f4034e804d6f1
AAlby99 <albyrahmadani4@gmail.com>
2023-08-23 13:37:09 +0700

    doujin

--- frontend/src/components/content/grafikAdmin.jsx
+++ frontend/src/components/content/grafikAdmin.jsx
@@ -1,8 +1,5 @@
-import React, { useEffect } from 'react';
-import {
-  Chart,
-  initTE,
-} from 'tw-elements';
+import React, { useEffect } from "react";
+import { Chart, initTE } from "tw-elements";
 
 initTE({ Chart });
 
@@ -10,34 +7,42 @@ const Tc = () => {
   useEffect(() => {
     // Chart data
     const dataLine = {
-      type: 'line',
+      type: "line",
       data: {
-        labels: ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'],
+        labels: [
+          "Senin",
+          "Selasa",
+          "Rabu",
+          "Kamis",
+          "Jumat",
+          "Sabtu",
+          "Minggu",
+        ],
         datasets: [
           {
-            label: 'Kategori 1',
+            label: "Kategori 1",
             data: [2, 4, 6, 8, 6, 4, 2],
-            borderColor: 'blue',
-            backgroundColor: 'rgba(0, 0, 255, 0.1)',
+            borderColor: "blue",
+            backgroundColor: "rgba(0, 0, 255, 0.1)",
           },
           {
-            label: 'Kategori 2',
+            label: "Kategori 2",
             data: [5, 5, 0, 2, 7, 8, 4],
-            borderColor: 'green',
-            backgroundColor: 'rgba(0, 255, 0, 0.1)',
+            borderColor: "green",
+            backgroundColor: "rgba(0, 255, 0, 0.1)",
           },
           {
-            label: 'Kategori 3',
+            label: "Kategori 3",
             data: [8, 5, 2, 6, 2, 5, 8],
-            borderColor: 'red',
-            backgroundColor: 'rgba(255, 0, 0, 0.1)',
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.1)",
           },
         ],
       },
     };
 
     // Create and render the chart
-    new Chart(document.getElementById('line-chart'), dataLine);
+    new Chart(document.getElementById("line-chart"), dataLine);
   }, []);
 
   return (
@@ -48,4 +53,3 @@ const Tc = () => {
 };
 
 export default Tc;
-
